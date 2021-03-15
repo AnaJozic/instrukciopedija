c85904a4097dfd242d9d76108d8d56b04e93810e
Anamarija Jozić <ana.jossic@gmail.com>
2021-03-15 10:39:29 +0100

    dodala jos neke detalje

--- app/src/main/java/com/example/instrukciopedijaapp/SubjectChoicesActivity.java
+++ app/src/main/java/com/example/instrukciopedijaapp/SubjectChoicesActivity.java
@@ -2,13 +2,27 @@ package com.example.instrukciopedijaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubjectChoicesActivity extends AppCompatActivity {
+
+    Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_choices);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SubjectChoicesActivity.this, RegisterUserActivity.class));
+            }
+        });
+
     }
 }
